58fd4c957fa3ada1e1ea92fbb05532d28f8a0afe
bhawansh1 <93114201+bhawansh1@users.noreply.github.com>
2026-05-16 20:28:50 +0530

    Clean up comments in InvalidAmount.java
    Removed unnecessary comments from InvalidAmount class.

--- src/com/bank/exception/InvalidAmount.java
+++ src/com/bank/exception/InvalidAmount.java
@@ -6,10 +6,6 @@ import com.bank.ui.*;
 import com.bank.util.*;
 
 public class InvalidAmount extends Exception {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	public InvalidAmount(String s)
 	{
