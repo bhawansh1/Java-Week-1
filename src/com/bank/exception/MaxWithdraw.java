c0ef6d3788bed95e48093cd6322090bd8c4cda29
bhawansh1 <93114201+bhawansh1@users.noreply.github.com>
2026-05-16 20:29:13 +0530

    Clean up MaxWithdraw.java by removing comments
    Removed unnecessary comments and whitespace in MaxWithdraw.java.

--- src/com/bank/exception/MaxWithdraw.java
+++ src/com/bank/exception/MaxWithdraw.java
@@ -6,12 +6,6 @@ import com.bank.ui.*;
 import com.bank.util.*;
 
 public class MaxWithdraw extends Exception {
-
-
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public MaxWithdraw(String s)
